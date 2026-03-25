de80bff445385d2c81aa68fb0311c3d0c13e0948
lSOOAH <lllssaa0722@gmail.com>
2026-03-25 18:16:07 +0900

    주석 내용 추가

--- Hello.java
+++ Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public void print(String name) {
 		System.out.println("Hello, " + name);
 	}
-
+  //진입 메서드
 	public static void main(String[] args) {
 		
 		Hello hello = new Hello();
